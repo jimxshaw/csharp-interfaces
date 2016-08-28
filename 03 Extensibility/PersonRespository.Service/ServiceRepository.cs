18c68b36e07b5006c17a3c6f66d9d1b6cea5e187
Jim Shaw <jimxshaw@gmail.com>
2016-08-28 13:53:21 -0500

    Implemented all methods within ServiceRespository.

--- 03 Extensibility/PersonRespository.Service/ServiceRepository.cs
+++ 03 Extensibility/PersonRespository.Service/ServiceRepository.cs
@@ -1,38 +1,45 @@
 ﻿using PersonRepository.Interface;
+using PersonRespository.Service.MyService;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PersonRespository.Service
 {
+    // This ServiceRepository class simply serves as a pass-thru to the WCF service data layer.
     public class ServiceRepository : IPersonRepository
     {
+        PersonServiceClient serviceProxy = new PersonServiceClient();
+
         public void AddPerson(Person newPerson)
         {
-            throw new System.NotImplementedException();
+            serviceProxy.AddPerson(newPerson);
         }
 
         public IEnumerable<Person> GetPeople()
         {
-            throw new System.NotImplementedException();
+            return serviceProxy.GetPeople();
         }
 
         public Person GetPerson(string lastName)
         {
-            throw new System.NotImplementedException();
-        }
-
-        public void UpdatePerson(string lastName, Person updatedPerson)
-        {
-            throw new System.NotImplementedException();
+            return serviceProxy.GetPerson(lastName);
         }
 
         public void UpdatePeople(IEnumerable<Person> updatedPeople)
         {
-            throw new System.NotImplementedException();
+            // Collections of type IEnumerable<T> can be converted to List<T> by
+            // calling ToList on the collection. 
+            serviceProxy.UpdatePeople(updatedPeople.ToList());
+        }
+
+        public void UpdatePerson(string lastName, Person updatedPerson)
+        {
+            serviceProxy.UpdatePerson(lastName, updatedPerson);
         }
 
         public void DeletePerson(string lastName)
         {
-            throw new System.NotImplementedException();
+            serviceProxy.DeletePerson(lastName);
         }
     }
 }
