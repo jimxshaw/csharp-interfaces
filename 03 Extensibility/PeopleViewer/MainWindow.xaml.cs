2ad5dba8044cfc82cfd385079fdb1fcb45cf4c7c
Jim Shaw <jimxshaw@gmail.com>
2016-08-28 14:52:51 -0500

    Brought in PersonRepository.SQL from folder. Added reference to it from PeopleView project. Implemented SQLFetchButton_Click method in MainWindow.xaml.cs

--- 03 Extensibility/PeopleViewer/MainWindow.xaml.cs
+++ 03 Extensibility/PeopleViewer/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using PersonRepository.CSV;
 using PersonRepository.Interface;
 using PersonRepository.Service;
+using PersonRepository.SQL;
 using System.Windows;
 
 namespace PeopleViewer
@@ -42,7 +43,16 @@ namespace PeopleViewer
 
         private void SQLFetchButton_Click(object sender, RoutedEventArgs e)
         {
+            ClearListBox();
+            IPersonRepository repository = new SQLRepository();
+            var people = repository.GetPeople();
 
+            foreach (var person in people)
+            {
+                PersonListBox.Items.Add(person);
+            }
+
+            ShowRepositoryType(repository);
         }
 
         private void ClearButton_Click(object sender, RoutedEventArgs e)
